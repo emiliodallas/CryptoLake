63b9f0ebc768080036dbdfa90daec909685c6cb9
Emilio <emiliodallas@hotmail.com>
2023-09-01 20:24:34 -0300

    Removed unnecessary files

--- get_crypto_info_by_ids/main.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-def get_crypto_info_by_ids(id):
-  url = 'https://pro-api.coinmarketcap.com/v2/cryptocurrency/quotes/latest'
-  headers, parameters = create_api_request_params(id)
-
-  response = requests.get(url, headers=headers, params=parameters)
-  return response.json()
--- get_current_time/main.py
+++
@@ -1,5 +0,0 @@
-from datetime import datetime
-
-def get_current_time():
-   current_datetime = datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
-   return current_datetime
