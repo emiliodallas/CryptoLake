a64b504489cd4db77fbc7a44ebc39221ce590895
Emilio <emiliodallas@hotmail.com>
2023-09-03 17:21:00 -0300

    Modularizing exrtact_to_storage

--- extract_to_storage/main.py
+++ extract_to_storage/main.py
@@ -1,8 +1,5 @@
 import pipeline_class
 
-crypto_ids = [1, 1027, 825]
-coin_names = ['Bitcoin', 'Ethereum', 'Tether']
-
 def caller(context):
     pipeline = pipeline_class.CryptomarketPipeline(project_id='cadastrachallenge', key_id='criptomarket_api_key', bucket_id='crypto-cap-market')
     pipeline.extract_api_data()   
